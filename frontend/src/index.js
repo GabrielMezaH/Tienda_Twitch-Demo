c62d036585981db57829ac86e947bdcb53ea68e8
gabriel <gabrieldesigner25@gmail.com>
2024-01-14 00:47:15 -0500

    Cambios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouteProvider
+  RouterProvider
 } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
@@ -12,11 +12,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 )
@@ -24,7 +26,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
